47df3d246aab6115999eabb351281138eff547d6
YASH CHOUDHARY <114938533+yashchoudhary110@users.noreply.github.com>
2024-01-23 17:35:57 +0000

    data summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+// primitive and non primitive
+
+// primitive (7 categories)
+//  these are call by value
+// String, Number, Boolean, null, Undefined, Symbol(to make value unique), BigInt
+
+// const score = false
+
+const id = Symbol('123')
+const anothrid = Symbol('123')
+
+console.log(id===anothrid);
+
+// Refernce (non primitve)
+// Array, objects, functions, 
